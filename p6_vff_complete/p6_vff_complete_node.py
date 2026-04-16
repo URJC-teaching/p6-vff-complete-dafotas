877ca44e5a283c6aff466e7663f01071b75698d7
dafauu <dafaucillox2005@gmail.com>
2026-04-16 10:32:12 +0200

    pkg changed, included repulsive consideration

--- p6_vff_complete/p6_vff_complete_node.py
+++ p6_vff_complete/p6_vff_complete_node.py
@@ -24,15 +24,15 @@ class VFFControllerNode(Node):
         # Parameters
         self.declare_parameter('max_linear_speed', 0.3)
         self.declare_parameter('max_angular_speed', 1.0)
-        #self.declare_parameter('repulsive_gain_factor', 1.0)
-        #self.declare_parameter('repulsive_influence_distance', 0.5)
-        #self.declare_parameter('stay_distance', -1.0) # -1.0 means no stay distance (2D case)
+        self.declare_parameter('repulsive_gain_factor', 1.0)
+        self.declare_parameter('repulsive_influence_distance', 0.5)
+        self.declare_parameter('stay_distance', 0.5) # -1.0 means no stay distance (2D case)
 
         self.max_linear_speed = self.get_parameter('max_linear_speed').value
         self.max_angular_speed = self.get_parameter('max_angular_speed').value
-        #self.repulsive_gain_factor = self.get_parameter('repulsive_gain_factor').value
-        #self.repulsive_influence_distance = self.get_parameter('repulsive_influence_distance').value
-        #self.stay_distance = self.get_parameter('stay_distance').value
+        self.repulsive_gain_factor = self.get_parameter('repulsive_gain_factor').value
+        self.repulsive_influence_distance = self.get_parameter('repulsive_influence_distance').value
+        self.stay_distance = self.get_parameter('stay_distance').value
 
         # Subscribers
         self.attractive_sub = self.create_subscription(
@@ -42,19 +42,19 @@ class VFFControllerNode(Node):
             10
         )
 
-        #self.repulsive_sub = self.create_subscription(
-        #    Vector3,
-        #    'repulsive_vector',
-        #    self.repulsive_callback,
-        #    10
-        #)
+        self.repulsive_sub = self.create_subscription(
+            Vector3,
+            'repulsive_vector',
+            self.repulsive_callback,
+            10
+        )
 
         # Publisher
         self.cmd_pub = self.create_publisher(Twist, 'vel', 10)
 
         # Internal state
         self.attractive_vec = Vector3()
-        #self.repulsive_vec = Vector3()
+        self.repulsive_vec = Vector3()
 
         self.state = "searching"
 
@@ -72,44 +72,43 @@ class VFFControllerNode(Node):
         self.attractive_vec = msg
         self.attractive_ts = self.get_clock().now()
         self.get_logger().debug(f'Received Attractive vector: x={msg.x:.2f}, y={msg.y:.2f}. Magnitude={math.hypot(msg.x, msg.y):.2f}. Angle={math.degrees(math.atan2(msg.y, msg.x)):.2f} deg')
-#
-    #def repulsive_callback(self, msg: Vector3):
-    #    self.repulsive_vec = msg
-    #    self.get_logger().debug(f'Received Repulsive vector: x={msg.x:.2f}, y={msg.y:.2f}. Magnitude={math.hypot(msg.x, msg.y):.2f}. Angle={math.degrees(math.atan2(msg.y, msg.x)):.2f} deg')
-    #    self.control_loop()
-#
+
+    def repulsive_callback(self, msg: Vector3):
+        self.repulsive_vec = msg
+        self.get_logger().debug(f'Received Repulsive vector: x={msg.x:.2f}, y={msg.y:.2f}. Magnitude={math.hypot(msg.x, msg.y):.2f}. Angle={math.degrees(math.atan2(msg.y, msg.x)):.2f} deg')
+
     def control_loop(self):
 
-        #if self.stay_distance > 0:
-        #    distance = math.hypot(self.attractive_vec.x, self.attractive_vec.y)
-        #    if distance < self.stay_distance:
-        #        self.get_logger().debug(f'Target @ {self.attractive_vec.x:.2f} m, {self.attractive_vec.y:.2f}. '
-        #                                f'Within stay distance ({distance:.2f} < {self.stay_distance}), ignoring attraction')
-        #        # self.attractive_vec = Vector3() # Zero attractive vector
-        #        return  # Do not move if within stay distance
-#
-        #obstacle_distance = math.hypot(self.repulsive_vec.x, self.repulsive_vec.y)
-        #
-        ## Initialize repulsive force components to zero
-        #repulsive_force_x = 0.0
-        #repulsive_force_y = 0.0
-#
-        #rho_0 = self.repulsive_influence_distance
-        #avoidance_needed = False
-        #if 0.0 < obstacle_distance <= rho_0: # If within influence distance
-        #    avoidance_needed = True
-        #    # Normally F_rep proportional to (1/d - 1/rho_0) but we kee it simple here
-#
-        #    force_mag_gain = 1.0 / obstacle_distance**2 # The closer the obstacle, the stronger the repulsive force
-     #
-        #    unit_x = self.repulsive_vec.x / obstacle_distance
-        #    unit_y = self.repulsive_vec.y / obstacle_distance
-#
-        #    repulsive_force_x = self.repulsive_gain_factor * force_mag_gain * unit_x
-        #    repulsive_force_y = self.repulsive_gain_factor * force_mag_gain * unit_y
-#
-        #    self.get_logger().debug(f'Repulsive magnitude={math.hypot(repulsive_force_x, repulsive_force_y):.2f}. Angle={math.degrees(math.atan2(self.repulsive_vec.y, self.repulsive_vec.x)):.2f} deg')
-#
+        if self.stay_distance > 0:
+            distance = math.hypot(self.attractive_vec.x, self.attractive_vec.y)
+            if distance < self.stay_distance:
+                self.get_logger().debug(f'Target @ {self.attractive_vec.x:.2f} m, {self.attractive_vec.y:.2f}. '
+                                        f'Within stay distance ({distance:.2f} < {self.stay_distance}), ignoring attraction')
+                # self.attractive_vec = Vector3() # Zero attractive vector
+                return  # Do not move if within stay distance
+
+        obstacle_distance = math.hypot(self.repulsive_vec.x, self.repulsive_vec.y)
+        
+        # Initialize repulsive force components to zero
+        repulsive_force_x = 0.0
+        repulsive_force_y = 0.0
+
+        rho_0 = self.repulsive_influence_distance
+        avoidance_needed = False
+        if 0.0 < obstacle_distance <= rho_0: # If within influence distance
+            avoidance_needed = True
+            # Normally F_rep proportional to (1/d - 1/rho_0) but we kee it simple here
+
+            force_mag_gain = 1.0 / obstacle_distance**2 # The closer the obstacle, the stronger the repulsive force
+     
+            unit_x = self.repulsive_vec.x / obstacle_distance
+            unit_y = self.repulsive_vec.y / obstacle_distance
+
+            repulsive_force_x = self.repulsive_gain_factor * force_mag_gain * unit_x
+            repulsive_force_y = self.repulsive_gain_factor * force_mag_gain * unit_y
+
+            self.get_logger().debug(f'Repulsive magnitude={math.hypot(repulsive_force_x, repulsive_force_y):.2f}. Angle={math.degrees(math.atan2(self.repulsive_vec.y, self.repulsive_vec.x)):.2f} deg')
+
         cmd = Twist()
         if self.check_person():
             self.state = "found_person"
@@ -123,8 +122,8 @@ class VFFControllerNode(Node):
             cmd.angular.z = self.max_angular_speed
             
         elif self.state == "found_person":
-                vff_x = self.attractive_vec.x 
-                vff_y = self.attractive_vec.y 
+                vff_x = self.attractive_vec.x - repulsive_force_x
+                vff_y = self.attractive_vec.y - repulsive_force_y
 
                 self.get_logger().info(f'VFF vector: x={vff_x:.2f}, y={vff_y:.2f}. Magnitude={math.hypot(vff_x, vff_y):.2f} Angle={math.degrees(math.atan2(vff_y, vff_x)):.2f} deg')
 
